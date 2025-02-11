3fe983d8198dde308886a54d2cd28981236c61bd
Mohammad Soqar <mnsoqar1@gmail.com>
2025-02-11 08:27:20 +0300

    Update glove.dart

--- lib/data/models/glove.dart
+++ lib/data/models/glove.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:rmts/data/models/abstract/abstract.dart';
+import 'package:rmts/data/models/enums/glove_status.dart';
 
 class Glove extends BaseModel {
-  final String? gloveId;
-  final String? status;
+  final String gloveId;
+  final GloveStatus status;
   final String? patientId;
   final String? model;
   final String? version;
@@ -22,9 +24,41 @@ class Glove extends BaseModel {
     this.lastUpdated,
     this.lastCalibrated,
   });
+
+  static Glove fromSnap(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, dynamic>;
+
+    GloveStatus parseStatus(String? status) {
+      return GloveStatus.values.firstWhere(
+        (e) => e.toString().split('.').last == status,
+        orElse: () => GloveStatus.idle,
+      );
+    }
+
+    return Glove(
+      gloveId: snap.id,
+      status: parseStatus(snapshot['status'] as String?),
+      patientId: snapshot['patientId'] ?? '',
+      model: snapshot['model'] ?? '',
+      version: snapshot['version'] ?? '',
+      productionDate: snapshot['productionDate'] ?? '',
+      lastSynced: snapshot['lastSynced'] ?? '',
+      lastUpdated: snapshot['lastUpdated'] ?? '',
+      lastCalibrated: snapshot['lastCalibrated'] ?? '',
+    );
+  }
+
   @override
   Map<String, dynamic> toJson() {
-    // TODO: implement toJson
-    throw UnimplementedError();
+    return {
+      'status': status.toString().split('.').last,
+      'patientId': patientId,
+      'model': model,
+      'version': version,
+      'productionDate': productionDate,
+      'lastSynced': lastSynced,
+      'lastUpdated': lastUpdated,
+      'lastCalibrated': lastCalibrated,
+    };
   }
 }
