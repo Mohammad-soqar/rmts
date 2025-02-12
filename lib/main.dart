c02384e21b93fe8a79d7a2ee084b425b6be2db11
Mohammad Soqar <mnsoqar1@gmail.com>
2025-02-12 16:53:05 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rmts/data/repositories/glove_repository.dart';
-import 'package:rmts/ui/views/register_view.dart';
+import 'package:rmts/ui/views/auth/login_view.dart';
+import 'package:rmts/ui/views/home_view.dart';
+import 'package:rmts/viewmodels/auth/auth_viewmodel.dart';
+import 'package:rmts/viewmodels/auth/register_viewmodel.dart';
 import 'package:rmts/viewmodels/glove_viewmodel.dart';
-import 'package:rmts/viewmodels/register_viewmodel.dart';
 
 import 'viewmodels/reports_viewmodel.dart';
 
@@ -17,6 +19,8 @@ void main() async {
   runApp(
     MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (_) => AuthViewModel()), // Auth Provider
+
         ChangeNotifierProvider(
             create: (_) => RegisterViewModel()), // RegisterViewModel Provider
         ChangeNotifierProvider(
@@ -41,7 +45,23 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const RegisterView(), // Set RegisterView as the initial screen
+      home: const AuthWrapper(), // Decide screen based on user authentication
     );
+  }
+}
+
+// This widget determines if the user is logged in or not
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final authViewModel = Provider.of<AuthViewModel>(context);
+
+    if (authViewModel.currentUser == null) {
+      return LoginView(); // If no user is logged in, show login screen
+    } else {
+      return HomeView(); // If user is logged in, show home screen
+    }
   }
 }
