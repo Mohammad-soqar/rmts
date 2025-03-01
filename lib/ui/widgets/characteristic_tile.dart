d7654a1f1ab1311573079410dbae1ae0c917f506
Mohammad Soqar <mnsoqar1@gmail.com>
2025-03-02 00:42:20 +0300

    Update characteristic_tile.dart

--- lib/ui/widgets/characteristic_tile.dart
+++ lib/ui/widgets/characteristic_tile.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
-import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
-import 'package:rmts/utils/snackbar.dart';
+
 import "descriptor_tile.dart";
 
 class CharacteristicTile extends StatefulWidget {
@@ -10,10 +10,10 @@ class CharacteristicTile extends StatefulWidget {
   final List<DescriptorTile> descriptorTiles;
 
   const CharacteristicTile({
-    Key? key,
+    super.key,
     required this.characteristic,
     required this.descriptorTiles,
-  }) : super(key: key);
+  });
 
   @override
   State<CharacteristicTile> createState() => _CharacteristicTileState();
@@ -29,7 +29,7 @@ class _CharacteristicTileState extends State<CharacteristicTile> {
     _lastValueSubscription =
         widget.characteristic.lastValueStream.listen((value) {
       String receivedData = String.fromCharCodes(value);
-      double parsedValue = double.tryParse(receivedData) ?? 2.0;
+      double parsedValue = double.tryParse(receivedData) ?? 0.0;
       print("Received raw value: $receivedData, Parsed value: $parsedValue");
       if (mounted) {
         setState(() {
@@ -49,32 +49,16 @@ class _CharacteristicTileState extends State<CharacteristicTile> {
 
   Future<void> sendCommand(String command) async {
     try {
-      await c.write(command.codeUnits, withoutResponse: c.properties.writeWithoutResponse);
+      await c.write(command.codeUnits,
+          withoutResponse: c.properties.writeWithoutResponse);
     } catch (e) {
       print(e);
     }
   }
 
-  Widget buildStartVibratorButton() {
-    return TextButton(
-      child: const Text("Start Vibrator"),
-      onPressed: () async {
-        await sendCommand("start");
-      },
-    );
-  }
-
-  Widget buildStopVibratorButton() {
-    return TextButton(
-      child: const Text("Stop Vibrator"),
-      onPressed: () async {
-        await sendCommand("stop");
-      },
-    );
-  }
-
   Widget buildUuid(BuildContext context) {
     String uuid = '0x${widget.characteristic.uuid.toString().toUpperCase()}';
+    print(widget.characteristic.uuid);
     return Text(uuid, style: const TextStyle(fontSize: 13));
   }
 
@@ -83,33 +67,115 @@ class _CharacteristicTileState extends State<CharacteristicTile> {
     return Text(data, style: const TextStyle(fontSize: 13, color: Colors.grey));
   }
 
-  Widget buildButtonRow(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        buildStartVibratorButton(),
-        buildStopVibratorButton(),
-      ],
+  Widget buildCommandButton(
+      BuildContext context, String label, String command, IconData icon) {
+    return ElevatedButton.icon(
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12), // Rounded corners
+        ),
+        backgroundColor: Colors.blueAccent, // Change the color as needed
+        foregroundColor: Colors.white, // Text and icon color
+        elevation: 4, // Shadow effect
+      ),
+      icon: Icon(color: Colors.white, icon, size: 20),
+      label: Text(label,
+          style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+      onPressed: () async {
+        await sendCommand(command);
+      },
+    );
+  }
+
+  Widget buildButtonGrid(BuildContext context) {
+    final List<Map<String, dynamic>> buttons = [
+      {
+        "label": "Start Vibrator",
+        "command": "startVibrator",
+        "icon": Icons.vibration
+      },
+      {
+        "label": "Stop Vibrator",
+        "command": "stopVibrator",
+        "icon": Icons.vibration
+      },
+      {"label": "Start MPU", "command": "startMPU", "icon": Icons.sensors},
+      {"label": "Stop MPU", "command": "stopMPU", "icon": Icons.sensors_off},
+      {"label": "Start MLX", "command": "startMLX", "icon": Icons.thermostat},
+      {
+        "label": "Stop MLX",
+        "command": "stopMLX",
+        "icon": Icons.thermostat_auto
+      },
+      {
+        "label": "Start PPG",
+        "command": "startPPG",
+        "icon": Icons.monitor_heart
+      },
+      {"label": "Stop PPG", "command": "stopPPG", "icon": Icons.heart_broken},
+      {"label": "Start FSR", "command": "startFSR", "icon": Icons.touch_app},
+      {"label": "Stop FSR", "command": "stopFSR", "icon": Icons.block},
+      {"label": "Start Flex", "command": "startFlex", "icon": Icons.swap_vert},
+      {
+        "label": "Stop Flex",
+        "command": "stopFlex",
+        "icon": Icons.swap_horizontal_circle
+      },
+    ];
+
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: GridView.builder(
+        shrinkWrap: true,
+        physics:
+            const NeverScrollableScrollPhysics(), // Prevents scroll conflicts
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, // Two buttons per row
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+          childAspectRatio: 3, // Adjust button size
+        ),
+        itemCount: buttons.length,
+        itemBuilder: (context, index) {
+          final button = buttons[index];
+          if (widget.characteristic.uuid.toString() ==
+              '6e400002-b5a3-f393-e0a9-e50e24dcca9e')
+            return buildCommandButton(
+                context, button["label"], button["command"], button["icon"]);
+          else
+            return Container();
+        },
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return ExpansionTile(
-      title: ListTile(
-        title: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    return Card(
+      margin: const EdgeInsets.all(8.0),
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const Text('Characteristic'),
+            const Text('Characteristic',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 8),
             buildUuid(context),
+            const SizedBox(height: 8),
             buildValue(context),
+            const SizedBox(height: 16),
+            buildButtonGrid(context),
+            const SizedBox(height: 16),
+            ...widget.descriptorTiles,
           ],
         ),
-        subtitle: buildButtonRow(context),
-        contentPadding: const EdgeInsets.all(0.0),
       ),
-      children: widget.descriptorTiles,
     );
   }
 }
