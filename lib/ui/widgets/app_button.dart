11ce34030f770167fb4a3c5d38eb8eda7b7188d0
Mohammad Soqar <mnsoqar1@gmail.com>
2025-03-03 13:05:08 +0300

    Update app_button.dart

---
+++ lib/ui/widgets/app_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final Color color;
+  final String label;
+  final VoidCallback onPressed;
+
+  const CustomButton({
+    super.key,
+    required this.color,
+    required this.label,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        label,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
