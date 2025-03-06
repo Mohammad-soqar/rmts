631576c4a0cab7071827a18ca0622343f1dc2bad
shamsa19 <shamsalim22@gmail.com>
2025-03-06 15:51:46 +0300

    Login Update

--- lib/ui/views/auth/login_view.dart
+++ lib/ui/views/auth/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rmts/viewmodels/auth/auth_viewmodel.dart';
+import 'package:rmts/ui/widgets/app_text_field.dart';
 
 class LoginView extends StatefulWidget {
   @override
@@ -10,49 +11,59 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     final authViewModel = Provider.of<AuthViewModel>(context);
 
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
+      // appBar: AppBar(title: Text('Login')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            authViewModel.isLoading
-                ? CircularProgressIndicator()
-                : ElevatedButton(
-                    onPressed: () {
-                      authViewModel.signIn(
-                        emailController.text.trim(),
-                        passwordController.text.trim(),
-                      );
-                    },
-                    child: Text('Login'),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Form(
+              key:_formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/rmt_logo.png",
+                    fit: BoxFit.contain,
                   ),
-            if (authViewModel.errorMessage != null)
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Text(
-                  authViewModel.errorMessage!,
-                  style: TextStyle(color: Colors.red),
-                ),
+                  SizedBox(height: 30),
+
+                  AppTextField(
+                    labelText: 'Email or Phone Number',
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  SizedBox(height: 10),
+
+                  AppTextField(
+                    labelText: 'Password',
+                    controller: passwordController,
+                    isPassword: true,
+                  ),
+                  SizedBox(height: 10),
+
+                 if (authViewModel.errorMessage != null)
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Text(
+                        authViewModel.errorMessage!,
+                        style: TextStyle(color: Colors.red),
+                      ),
+                    ),
+                
+                ],
               ),
-          ],
+            ),
+          )
         ),
       ),
     );
   }
 }
+
+            
