d6a75927b438641e40a9f5495b052b4b76879bdd
Mohammad Soqar <mnsoqar1@gmail.com>
2025-03-12 07:19:32 +0300

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rmts/data/repositories/glove_repository.dart';
 import 'package:rmts/ui/themes/theme.dart';
-import 'package:rmts/ui/views/appointment_management/add_appointment_view.dart';
-import 'package:rmts/ui/views/appointment_management/appointment_view.dart';
 import 'package:rmts/ui/views/auth/splashScreens/SplashView.dart';
 import 'package:rmts/ui/views/home_view.dart';
 import 'package:rmts/viewmodels/appointment_viewmodel.dart';
@@ -49,17 +48,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'RMTS System',
-      theme: const MaterialTheme(TextTheme()).light(), // Apply Light Theme
-      darkTheme: const MaterialTheme(TextTheme()).dark(), // Apply Dark Theme
-      themeMode: ThemeMode.system, // Switch based on system theme
-      home: const AuthWrapper(), // Decide screen based on authentication
-      routes: {
-        '/home': (context) => const HomeView(),
-        '/viewAppointments': (context) =>
-            const AppointmentView(patientId: "patient123"),
-        '/addAppointment': (context) =>
-            const AddAppointmentView(patientId: "patient123"),
-      },
+      theme: const MaterialTheme(TextTheme()).light(), 
+      darkTheme: const MaterialTheme(TextTheme()).dark(), 
+      themeMode: ThemeMode.system, 
+      home: const AuthWrapper(), 
     );
   }
 }
@@ -70,12 +62,19 @@ class AuthWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final authViewModel = Provider.of<AuthViewModel>(context);
-
-    if (authViewModel.currentUser == null) {
-      return const SplashView(); // User is not logged in, show login screen
-    } else {
-      return const HomeView(); // User is authenticated, go to home screen
-    }
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+              body: Center(child: CircularProgressIndicator()));
+        } else {
+          if (snapshot.hasData) {
+            return const HomeView(); // Show Home Screen
+          }
+          return const SplashView(); // Show Splash Screen
+        }
+      },
+    );
   }
 }
