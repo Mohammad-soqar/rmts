6703daa88e15ab38cbd729a6c9a37dea8a9b29f5
Mohammad Soqar <mnsoqar1@gmail.com>
2025-05-06 09:38:12 +0300

    Update sensor_data_service.dart

--- lib/data/services/sensor_data_service.dart
+++ lib/data/services/sensor_data_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:rmts/data/models/hive/flex_data.dart';
 import 'package:rmts/data/models/hive/mpu_data.dart';
 import 'package:rmts/data/models/hive/ppg_data.dart';
 
@@ -10,8 +11,8 @@ class SensorDataService extends ChangeNotifier {
     await FirebaseFirestore.instance
         .collection('patients')
         .doc(patientId)
-        .collection('sensors')
-        .doc('ppg_data')
+        .collection('ppg_data')
+        .doc('ppg_data_${ppgdata.timestamp.millisecondsSinceEpoch}')
         .set({
       'bpm': ppgdata.bpm,
       'timestamp': ppgdata.timestamp.millisecondsSinceEpoch,
@@ -30,6 +31,20 @@ class SensorDataService extends ChangeNotifier {
       'lowered': mpudata.lowered,
       'raised': mpudata.raised,
       'timestamp': mpudata.timestamp.millisecondsSinceEpoch,
+    });
+
+    notifyListeners();
+  }
+
+  Future<void> addFlexData(FlexData flexData, String patientId) async {
+    await FirebaseFirestore.instance
+        .collection('patients')
+        .doc(patientId)
+        .collection('sensors')
+        .doc('flex_data')
+        .set({
+      'bent': flexData.bent,
+      'timestamp': flexData.timestamp.millisecondsSinceEpoch,
     });
 
     notifyListeners();
